e974d3c9707def88203ed4334fe8a8db1234743b
Diogo Silva <12896361+diogols@users.noreply.github.com>
2018-11-15 17:28:46 +0000

    added submenus hierarchy

--- UCalculator/src/controller/UCalculatorController.java
+++ UCalculator/src/controller/UCalculatorController.java
@@ -1,9 +1,8 @@
 package controller;
 
+import jdk.swing.interop.SwingInterOpUtils;
 import view.Menu;
 import view.UCalculatorView;
-
-import java.util.Arrays;
 
 public class UCalculatorController {
 
@@ -25,10 +24,13 @@ public class UCalculatorController {
 
                 switch (option) {
                     case "1":
+                        localDateTimeCalculator();
                         break;
                     case "2":
+                        timeZoneDateTimeCalculator();
                         break;
                     case "3":
+                        meetingSchedule();
                         break;
                     case "0":
                         break;
@@ -37,5 +39,55 @@ public class UCalculatorController {
                 }
             } while (!option.equals("0"));
         }
+    }
+
+    private void localDateTimeCalculator() {
+        Menu menu = view.getMenu(1);
+
+        if (menu != null) {
+            String option;
+
+            do {
+                menu.show();
+                option = Input.readString();
+
+                switch (option) {
+                    case "1":
+                        dateTimeCalculator();
+                        break;
+                    case "2":
+                        weeksCalculator();
+                        break;
+                    case "3":
+                        timeUnitsCalculator();
+                        break;
+                    case "0":
+                        break;
+                    default:
+                        System.out.println("Invalid option.");
+                }
+            } while (!option.equals("0"));
+        }
+    }
+
+    private void dateTimeCalculator() {
+        System.out.println("DATE TIME CALCULATOR HERE");
+    }
+
+    // TODO: João
+    private void weeksCalculator() {
+        System.out.println("WEEKS CALCULATOR HERE");
+    }
+
+    private void timeUnitsCalculator() {
+        System.out.println("TIME UNITS CALCULATOR");
+    }
+
+    private void timeZoneDateTimeCalculator() {
+        System.out.println("TIME ZONE DATE TIME CALCULATOR HERE");
+    }
+
+    private void meetingSchedule() {
+        System.out.println("MEETING SCHEDULE");
     }
 }
