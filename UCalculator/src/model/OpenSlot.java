5f7353e2c865d01dfa158dc7d134bf1451f9ec72
rafaelbraga25 <rafael.braga.gc@gmail.com>
2018-12-04 16:10:26 +0000

    Add comments

--- UCalculator/src/model/OpenSlot.java
+++ UCalculator/src/model/OpenSlot.java
@@ -3,12 +3,31 @@ package model;
 import java.io.Serializable;
 import java.time.LocalTime;
 
+/**
+ * Classe que representa um slot aberto que permite que lhe sejam adicioanadas tarefas.
+ *
+ * @author Diogo Silva
+ * @author João Barreira
+ * @author Rafael Braga
+ */
 public class OpenSlot extends Slot implements Serializable {
 
+    /**
+     * Inicializa um slot como um slot aberto.
+     *
+     * @param id        Id do slot.
+     * @param startTime Hora de início do slot.
+     * @param endTime   Hora de fim do slot.
+     */
     OpenSlot(int id, LocalTime startTime, LocalTime endTime) {
         super(id, startTime, endTime);
     }
 
+    /**
+     * Construtor de cópia.
+     *
+     * @param openSlot Objeto a ser copiado.
+     */
     OpenSlot(OpenSlot openSlot) {
         super(openSlot);
     }
