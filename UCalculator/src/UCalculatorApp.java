0ae865fc1340ad49c185332d27ccf07cb2e563b0
Diogo Silva <12896361+diogols@users.noreply.github.com>
2018-12-04 13:42:46 +0000

    Update start/end slot from time and finish

--- UCalculator/src/UCalculatorApp.java
+++ UCalculator/src/UCalculatorApp.java
@@ -8,10 +8,6 @@ import java.time.LocalTime;
 public class UCalculatorApp {
 
     public static void main(String[] args) {
-        Schedule schedule = new Schedule(60);
-        System.out.println(schedule.setStartSlot(LocalTime.of(8, 30)));
-        //bug here
-        System.out.println(schedule.setEndSlot(LocalTime.of(19, 30)));
         UCalculatorView view = new UCalculatorView();
         UCalculatorModel model = new UCalculatorModel();
         UCalculatorController controller = new UCalculatorController();
